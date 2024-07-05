e6e5f280f6ee1252c8de2b7cd95465a6ba5eda93
howyoujini <howyoujini@gmail.com>
2024-07-06 06:46:18 +0900

    Updates

---
+++ precache-manifest.50ab4048e22da5a389a76e153c223f7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a1fb2147ac2f678eb57ac65e171e0e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a435451c3021c53d1b55",
+    "url": "/portfolio/static/css/main.b8544b7f.chunk.css"
+  },
+  {
+    "revision": "32c053ec7abe0b09e7c0",
+    "url": "/portfolio/static/js/2.dd375acf.chunk.js"
+  },
+  {
+    "revision": "2f9582847c006e291c56",
+    "url": "/portfolio/static/js/3.5d4f8254.chunk.js"
+  },
+  {
+    "revision": "a435451c3021c53d1b55",
+    "url": "/portfolio/static/js/main.58eb5f13.chunk.js"
+  },
+  {
+    "revision": "ac7008fae149db93f5c2",
+    "url": "/portfolio/static/js/runtime~main.a6c851fc.js"
+  }
+]);
